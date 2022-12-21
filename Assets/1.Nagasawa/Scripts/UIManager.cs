432c9c653d35f45c7b19518d8afd4310a01e8687
NagasawaYusei <curga3013@gmail.com>
2022-12-21 14:52:41 +0900

    [Add] UIManager作った

---
+++ Assets/1.Nagasawa/Scripts/UIManager.cs
@@ -0,0 +1,18 @@
+using UnityEngine;
+using UnityEngine.UI;
+using System;
+
+public class UIManager : MonoBehaviour
+{
+    [SerializeField] TimeManager _tm;
+    [SerializeField] Text _timeText;
+    [SerializeField] Text _scoreTextOne;
+    [SerializeField] Text _scoreTextTwo;
+
+    void Update()
+    {
+        _timeText.text = _tm.CurrentTime.ToString("F2");
+        _scoreTextOne.text = String.Format("{0:D8}", GameManager.Score[0]);
+        _scoreTextTwo.text = String.Format("{0:D8}", GameManager.Score[1]);
+    }
+}
